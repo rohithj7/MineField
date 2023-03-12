ae75dc6460da38a7caf6b78cf3e658df5448ae51
jelinnne <127637901+jelinnne@users.noreply.github.com>
2023-03-12 00:23:04 -0800

    Add files via upload

---
+++ mvc/AppFactory.java
@@ -0,0 +1,12 @@
+package mvc;
+
+public interface AppFactory {
+    public Model makeModel();
+    public View makeView();
+    public String getTitle();
+    public String[] getHelp();
+    public String about();
+    public String[] getEditCommands();
+    public Command makeEditCommand(String name);
+
+}
