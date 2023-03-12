3e05988ed680218acb0cfadd3dd6110d744c2398
Nathan Duong <100316459+natooni@users.noreply.github.com>
2023-03-11 18:42:21 -0800

    Add files via upload

---
+++ mvc/AppPanel.java
@@ -0,0 +1,13 @@
+package mvc;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AppPanel extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
