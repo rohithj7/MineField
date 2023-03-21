0158d10d5628b23d0deeeb25e5cf543c7036cfa8
Nathan Duong <100316459+natooni@users.noreply.github.com>
2023-03-20 18:40:12 -0700

    Add files via upload

--- MineField/MoveCommand.java
+++ MineField/MoveCommand.java
@@ -1,13 +1,16 @@
-package MineField;
-import mvc.*;
-public class  MoveCommand extends Command{
-    private MineField field;
+package mineField;
+
+import mvc.Command;
+import mvc.Model;
+
+public class MoveCommand extends Command {
+    private final MineField field;
     private int x, y;
 
     public MoveCommand(Model m) {
 
         super(m);
-        this.field = (MineField)m;
+        this.field = (MineField) m;
     }
 
     public void setMove(int x, int y) {
